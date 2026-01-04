d7718045c49bff86a974c8ceb2fe73a0834ec75e
smattiza <smattiza@gmail.com>
2026-01-04 11:55:25 -0600

    Fix child theme stylesheet enqueue for block theme compatibility

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style(
         'awenarts-child-style',
         get_stylesheet_uri(),
-        ['twentytwentyfour-style'],
+        [],
         wp_get_theme()->get('Version')
     );
-});
+}, 20);
